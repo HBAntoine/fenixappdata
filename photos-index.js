1066b85989afd851fcfefb3e8aab9f9800169c6a
HBAntoine <antoine.magnard@sfr.fr>
2026-02-16 00:30:26 +0100

    MAJ photos Limoges 15/02/2026 23:30 (-1 photo(s))

--- photos-index.js
+++ photos-index.js
@@ -101,7 +101,6 @@ var PHOTOS_INDEX = {
     "97 Al-Khafadji.png"
   ],
   "Limoges": [
-    "azkue-jon__picture__2025-2026-7705-15827.png",
     "clarac-theo__picture__2025-2026-4333-16308.png",
     "garcia-arnau__picture__2025-2026-2799-16084.png",
     "guillaume-andrea__picture__2025-2026-3124-15835.png",
